d3347c9a808a056343c328350a0dbd7712007a95
Anibal Andres Laz <andylaz92@outlook.com>
2023-09-27 23:12:50 -0300

    añadi carpetas views,routers,js,models, etc

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
+// Importación de librerías
 const express = require('express');
 const cors = require ('cors');
 const morgan = require ('morgan');
+const path = require ('path');
+
+require('ejs')
+require('dotenv').config();
 
 
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
+// Middlewares
 app.use(cors())
 app.use(morgan('combined'))
 app.use(express.json())
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+//archivos estaticos
+app.use( express.static(path.join(__dirname, 'public')) )
 
-app.post('/', function (req, res) {
-  res.send('Hello World')
-})
+// Motor de plantillas
+app.set('view engine', 'ejs')
+
+//Rutas
+app.use(require('./routers/blog.routes'))
+app.use(require('./routers/user.routes'))
 
 app.listen(port, () => console.log(`Servidor en http://localhost:${port}`))
 
